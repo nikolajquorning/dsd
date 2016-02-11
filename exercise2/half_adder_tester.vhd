0fa8c7e402354b8097df1a7e3cfc25bb8cf71cc6
Rasmus V. Plauborg <rplauborg@gmail.com>
2016-02-11 10:16:25 +0100

    Exercise2 start

---
+++ exercise2/half_adder_tester.vhd
@@ -0,0 +1,13 @@
+-- File: half_adder_tester.vhd
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity half_adder_tester is
+	port(	SW		:	in std_logic_vector(1 downto 0);
+			LEDR	:	out std_logic_vector(1 downto 0));
+end half_adder_tester;
+
+architecture struc of half_adder_tester is
+begin 
+i1: entity work.half_adder port map(a => SW(0), b => SW(1), sum => LEDR(0), carry_out => LEDR(1));
+end struc;
