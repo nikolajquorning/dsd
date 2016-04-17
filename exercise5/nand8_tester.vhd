4bf60ba0863c71bb9c4bbc31daaf6c092a2340a6
Nikolaj Quorning <nikolaj.quorning@gmail.com>
2016-04-17 14:11:50 +0200

    Fuck DSD

---
+++ exercise5/nand8_tester.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity nand8_tester is
+	port (
+		SW    : in  std_logic_vector(7 downto 0);
+		LEDR0 : out std_logic
+	);
+end nand8_tester;
+
+architecture testing of nand8_tester is
+	begin
+	i0: entity work.nand8
+		port map (
+			a(7 downto 0) => SW(7 downto 0),
+			y => LEDR0
+		);
+end testing;
